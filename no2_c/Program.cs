bbd3eb1205aca5c7a0c3353d103ada581db39aac
fahrizakarina <fahrikeren1406@gmail.com>
2020-04-26 10:01:22 +0700

    pertama

---
+++ no2_c/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no2_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RetailItem item1 = new RetailItem("Jacket", 17, 59.95);
+            RetailItem item2 = new RetailItem("Designer Jeans", 17, 59.95);
+            RetailItem item3 = new RetailItem("Shirt", 17, 59.95);
+
+            //item1.setDescription("Watch");
+
+            Console.WriteLine("\t\tDescription\tUnitsOnHand\tPrice");
+            Console.WriteLine("item #1\t\t" + item1.getDescription() + "\t\t" + item1.getUnitsOnHand() + "\t\t" + item1.getPrice());
+            Console.WriteLine("item #2\t\t" + item2.getDescription() + "\t" + item2.getUnitsOnHand() + "\t\t" + item2.getPrice());
+            Console.WriteLine("item #3\t\t" + item3.getDescription() + "\t\t" + item3.getUnitsOnHand() + "\t\t" + item3.getPrice());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ no2_c/RetailItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no2_c
+{
+    class RetailItem
+    {
+        private String description;
+        private int unitsOnHand;
+        private Double price;
+
+        public void setDescription(String userDescription) 
+        {
+            description = userDescription;
+        }
+        public void setUnitsOnHand(int userUnitsOnHand)
+        {
+            unitsOnHand = userUnitsOnHand;
+        }
+        public void setPice(Double userPrice)
+        {
+            price = userPrice;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+        public int getUnitsOnHand()
+        {
+            return unitsOnHand;
+        }
+        public Double getPrice()
+        {
+            return price;
+        }
+
+        public RetailItem(String descriptionGiven, int unitsOnHanGiven, Double priceGiven) 
+        {
+            description = descriptionGiven;
+            unitsOnHand = unitsOnHanGiven;
+            price = priceGiven;
+        }
+    }
+}
